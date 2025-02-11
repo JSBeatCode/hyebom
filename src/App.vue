9db3f6c9515a0835c7f779bdcd10ab0b3023b904
JS <csdnum0@gmail.com>
2025-02-12 01:31:31 +0900

    d

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,9 @@ onMounted(() => {
 </script>
 
 <template>
-  {{ refSrcData.assetsVueSvg }}
+  <div>
+    deploye-v2
+  </div>
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
