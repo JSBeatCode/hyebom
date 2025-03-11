019f2743f48fad58c27004cdf6edf576cf254cef
JS <csdnum0@gmail.com>
2025-03-11 23:23:15 +0900

    d

--- src/views/Prices.vue
+++ src/views/Prices.vue
@@ -1,7 +1,92 @@
 <template>
-    <div style="height: 500px;">
-        <h1>Prices</h1>
+<main class="main">
+
+<!-- Page Title -->
+<div class="page-title dark-background" data-aos="fade" style="background-image: url(@/assets/img/page-title-bg.webp);">
+  <div class="container position-relative">
+    <h1>수수료 안내</h1>
+    <p>"투명하고 합리적인 수수료로 최적의 세무·회계 서비스를 제공합니다."</p>
+    <!-- <nav class="breadcrumbs">
+      <ol>
+        <li><a href="index.html">Home</a></li>
+        <li class="current">Price Details</li>
+      </ol>
+    </nav> -->
+  </div>
+</div><!-- End Page Title -->
+
+<!-- Price Details Section -->
+<section id="price-details" class="price-details section">
+
+  <div class="container">
+
+    <div class="row gy-4">
+
+      <div class="col-lg-4" data-aos="fade-up" data-aos-delay="100">
+        <!-- <h4>투명하고 합리적인 수수료</h4> -->
+        <!-- <p>혜봄세무회계는 투명하고 합리적인 수수료로 최적의 세무·회계 서비스를 제공합니다. 상세한 요금은 상담 후 안내해 드립니다.</p> -->
+        <div class="price-list">
+          <span id="price-button-1" onclick="showPrice(1)" class="active">사업자 세무회계</span>
+          <span id="price-button-2" onclick="showPrice(2)">재산제세</span>
+        </div>
+        
+      </div>
+
+      <div id="price-1" class="col-lg-8" data-aos="fade-up" data-aos-delay="200">
+        <img src="@/assets/img/services-1.jpg" alt="" class="img-fluid services-img" style="height: 50%">
+        <h3>사업자 세무회계</h3>
+        <!-- <p>
+          사업자 세무회계 서비스는 세무 신고, 재무 분석, 세금 절감 방안 등 사업 운영에 필수적인 세무 관리 전반을 지원합니다. 혜봄세무회계는 사업자 맞춤형 솔루션을 제공하여, 복잡한 세무 업무를 간편하게 처리하고, 세무 리스크를 최소화하며, 안정적인 사업 성장을 돕습니다.
+        </p> -->
+        <ul>
+          <li><i class="bi bi-check-circle"></i> <span>월 기장 상담 (매월 세무관리)</span></li>
+          <li><i class="bi bi-check-circle"></i> <span>기존 사업자를 위한 세금 상담.</span></li>
+          <li><i class="bi bi-check-circle"></i> <span>창업자를 위한 전문 세무 상담</span></li>
+        </ul>
+        <p>
+          월 기장 상담시, 상황에 맞는 세무 planning을 제공하므로 최초 상담료는 발생합니다. 
+        </p>
+        <p>
+          ※ 단순 견적문의는 견적서를 요청해주세요!
+        </p>
+        <!-- <p>
+          사업을 운영하다보면 사업 세무회계에 대해 궁금한게 많습니다. 당연히 세무 서비스는 매월 꾸준히 관리하는 것이 좋겠지만, 윌 기장은 부담스럽고, 인터넷 검색도 힘듭니다. 이제 전문가인 혜봄세무회계에 맡기십시오.
+        </p>
+        <p>
+          처음 사업자등록증 표기에 따라 세금이 달라진다는 걸 알고 있으세요? 창업을 준비하신다면, 꼭 필요한 세무 상담!
+        </p> -->
+      </div>
+
+      <div id="price-2" class="col-lg-8 hiddenDisplay" data-aos="fade-up" data-aos-delay="200">
+        <img src="@/assets/img/services-2.jpg" alt="" class="img-fluid services-img" style="height: 50%">
+        <h3>재산제세</h3>
+        <!-- <p>
+          2 Blanditiis voluptate odit ex error ea sed officiis deserunt. Cupiditate non consequatur et doloremque consequuntur. Accusantium labore reprehenderit error temporibus saepe perferendis fuga doloribus vero. Qui omnis quo sit. Dolorem architecto eum et quos deleniti officia qui.
+        </p> -->
+        <ul>
+          <li><i class="bi bi-check-circle"></i> <span>재산관련 세금 상담.</span></li>
+          <!-- <li><i class="bi bi-check-circle"></i> <span>Assumenda et porro nisi nihil nesciunt voluptatibus.</span></li> -->
+          <!-- <li><i class="bi bi-check-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea</span></li> -->
+        </ul>
+        <p>
+          상속세, 증여세, 양도소득세, 종합부동산세 등등의 재산 상담을 원하시나요? 2인의 재산 전문 세무사가 고객이 원하는 방향으로 센스있게 상담합니다.
+        </p>
+        <p>
+          [고객 상황에 맞는 절세방향 + 리스크 안내 + 실무처리방법] 안내는 기본!
+        </p>
+        <p>
+          ※ 상담 이후 신고대리를 수임하실 경우 상담료는 차감됩니다.
+        </p>
+      </div>
+
     </div>
+
+  </div>
+
+</section>
+<!-- /Price Details Section -->
+
+</main>
 </template>
 
 <script setup>
--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,7 +1,139 @@
 <template>
-    <div style="height: 500px;">
-        <h1>Services</h1>
+  <main class="main">
+
+<!-- Page Title -->
+<div class="page-title dark-background" data-aos="fade" style="background-image: url(@/assets/img/page-title-bg.webp);">
+  <div class="container position-relative">
+    <h1>주요업무</h1>
+    <p>"사업 특성에 맞춘 재무 분석과 철저한 세무조사 대응으로 고객님의 자산을 안전하게 지켜드립니다."</p>
+    <!-- <nav class="breadcrumbs">
+      <ol>
+        <li><a href="index.html">Home</a></li>
+        <li class="current">Service Details</li>
+      </ol>
+    </nav> -->
+  </div>
+</div><!-- End Page Title -->
+
+<!-- Service Details Section -->
+<section id="service-details-1" class="portfolio-details section">
+
+  <div class="container" data-aos="fade-up" data-aos-delay="100">
+
+    <div class="row gy-4">
+
+      <div class="col-lg-4">
+        
+        <div class="portfolio-description" data-aos="fade-up" data-aos-delay="300">
+          <h2>기업의 성장과 안정성을 지원합니다.</h2>
+          <p>
+            (설명글) (예시)사업 특성에 맞춘 재무 분석과 전략적 세무 컨설팅을 드립니다. 수수료 안내(한국세무사회 보수표에 따름)는 참고해 주세요. 사엄자 세금(부가세, 원천세 등), 신고 및 세금관리에 대해 지원합니다.
+          </p>
+        </div>
+
+        <div class="portfolio-info" data-aos="fade-up" data-aos-delay="200">
+          <h3 style="color: #ff4a17">사업자 재무상담</h3>
+          <ul>
+            <li><strong>주요고객</strong>: 기업, 개인사업자, 프리랜서, 자영업</li>
+            <li><strong>상담료</strong>: 55,000원 (기장수임, 단순견적문의시 별도 상담료 없음)</li>
+            <li><strong>기장수임</strong>: 기장료(월) + 조정료(연1회) 발생</li>
+            <!-- <li>
+              <strong>설명</strong>: 
+              <p>수수료 안내(한국세무사회 보수표에 따름) 참고.</p>
+              <p>사엄자 세금(부가세, 원천세 등) 신고 및 세금관리.</p>
+            </li> -->
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-lg-8">
+        <div class="portfolio-details-slider swiper init-swiper">
+
+          <div class="swiper-wrapper align-items-center">
+
+            <div class="swiper-slide">
+              <img src="@/assets/img/portfolio/app-1.jpg" alt="">
+            </div>
+
+            <div class="swiper-slide">
+              <img src="@/assets/img/portfolio/product-1.jpg" alt="">
+            </div>
+
+            <div class="swiper-slide">
+              <img src="@/assets/img/portfolio/branding-1.jpg" alt="">
+            </div>
+
+            <div class="swiper-slide">
+              <img src="@/assets/img/portfolio/books-1.jpg" alt="">
+            </div>
+
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
+      </div>
+
     </div>
+  </div>
+
+</section><!-- /Service Details Section -->
+
+<!-- Service Details Section -->
+<section id="service-details-2" class="portfolio-details section">
+
+  <div class="container" data-aos="fade-up" data-aos-delay="100">
+    <div class="row gy-4">
+      <div class="col-lg-4">
+        
+        <div class="portfolio-description" data-aos="fade-up" data-aos-delay="300">
+          <h2>고객님의 자산을 안전하게 지켜드립니다.</h2>
+          <p>
+            (설명글)
+          </p>
+        </div>
+
+        <div class="portfolio-info" data-aos="fade-up" data-aos-delay="200">
+          <h3 style="color: #ff4a17">재산 & 세무조사</h3>
+          <ul>
+            <li><strong>주요고객</strong>: 기업, 개인사업자, 프리랜서, 자영업</li>
+            <li><strong>상담료</strong>: 110,000원 (30분 당. 단순견적문의시 별도 상담료 없음)</li>
+            <li><strong>신고대리 수임</strong>: 수임범위 및 사후관리 용역강도에 따라 수임료 상이</li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-lg-8">
+        <div class="portfolio-details-slider swiper init-swiper">
+
+
+          <div class="swiper-wrapper align-items-center">
+
+            <div class="swiper-slide">
+              <img src="@/assets/img/portfolio/books-1.jpg" alt="">
+            </div>
+
+            <div class="swiper-slide">
+              <img src="@/assets/img/portfolio/branding-1.jpg" alt="">
+            </div>
+            
+            <div class="swiper-slide">
+              <img src="@/assets/img/portfolio/product-1.jpg" alt="">
+            </div>
+
+            <div class="swiper-slide">
+              <img src="@/assets/img/portfolio/app-1.jpg" alt="">
+            </div>
+
+
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+</section><!-- /Service Details Section -->
+
+</main>
 </template>
 
 <script setup>
